bbd9585221ab7835f84e7e7c9714851b930d876a
masaki-kan <63283812+masaki-kan@users.noreply.github.com>
2020-10-27 22:31:14 +0900

    commit

--- resources/views/include/input_family.blade.php
+++ resources/views/include/input_family.blade.php
@@ -4,7 +4,7 @@
 @if( isset( $images->grandfather ) )
 <li>
   @if( $names->grandfather_img )
-<img class="img-thumbnail" src="{{ asset('storage/user/'. $names->grandfather_img) }}">
+<img class="img-thumbnail" src="{{ $names->grandfather_img_path }}">
 @else
 <img class="img-thumbnail" src="{{ asset('/sample_images/user_icon.png') }}">
 @endif
@@ -13,7 +13,7 @@
 @if( isset( $images->grandmother ) )
 <li>
   @if( $names->grandmother_img )
-<img class="img-thumbnail" src="{{ asset('storage/user/'. $names->grandmother_img) }}">
+<img class="img-thumbnail" src="{{ $names->grandmother_img_path }}">
 @else
 <img class="img-thumbnail" src="{{ asset('/sample_images/user_icon.png') }}">
 @endif
@@ -22,7 +22,7 @@
 @if( isset( $images->father ) )
 <li>
   @if( $names->father_img )
-<img class="img-thumbnail" src="{{ asset('storage/user/'. $names->father_img) }}">
+<img class="img-thumbnail" src="{{ $names->father_img_path }}">
 @else
 <img class="img-thumbnail" src="{{ asset('/sample_images/user_icon.png') }}">
 @endif
@@ -31,7 +31,7 @@
 @if( isset( $images->mather ) )
 <li>
   @if( $names->mather_img )
-<img class="img-thumbnail" src="{{ asset('storage/user/'. $names->mather_img) }}">
+<img class="img-thumbnail" src="{{ $names->mather_img_path }}">
 @else
 <img class="img-thumbnail" src="{{ asset('/sample_images/user_icon.png') }}">
 @endif
@@ -40,7 +40,7 @@
 @if( isset( $images->son_first_man ) )
 <li>
   @if( $names->son_first_man_img )
-<img class="img-thumbnail" src="{{ asset('storage/user/'. $names->son_first_man_img) }}">
+<img class="img-thumbnail" src="{{ $names->son_first_man_img_path }}">
 @else
 <img class="img-thumbnail" src="{{ asset('/sample_images/user_icon.png') }}">
 @endif
@@ -49,7 +49,7 @@
 @if( isset( $images->son_first_woman ) )
 <li>
   @if( $names->son_first_woman_img )
-<img class="img-thumbnail" src="{{ asset('storage/user/'. $names->son_first_woman_img) }}">
+<img class="img-thumbnail" src="{{ $names->son_first_woman_img_path }}">
 @else
 <img class="img-thumbnail" src="{{ asset('/sample_images/user_icon.png') }}">
 @endif
@@ -58,7 +58,7 @@
 @if( isset( $images->son_second_man ) )
 <li>
   @if( $names->son_second_man_img )
-<img class="img-thumbnail" src="{{ asset('storage/user/'. $names->son_second_man_img) }}">
+<img class="img-thumbnail" src="{{ $names->son_second_man_img_path }}">
 @else
 <img class="img-thumbnail" src="{{ asset('/sample_images/user_icon.png') }}">
 @endif
@@ -67,7 +67,7 @@
 @if( isset( $images->son_second_woman ) )
 <li>
   @if( $names->son_second_woman_img )
-<img class="img-thumbnail" src="{{ asset('storage/user/'. $names->son_second_woman_img) }}">
+<img class="img-thumbnail" src="{{ $names->son_second_woman_img_path }}">
 @else
 <img class="img-thumbnail" src="{{ asset('/sample_images/user_icon.png') }}">
 @endif
@@ -76,7 +76,7 @@
 @if( isset( $images->son_third_man ) )
 <li>
   @if( $names->son_third_man_img )
-<img class="img-thumbnail" src="{{ asset('storage/user/'. $names->son_third_man_img) }}">
+<img class="img-thumbnail" src="{{ $names->son_third_man_img_path }}">
 @else
 <img class="img-thumbnail" src="{{ asset('/sample_images/user_icon.png') }}">
 @endif
@@ -85,7 +85,7 @@
 @if( isset( $images->son_third_woman ) )
 <li>
   @if( $names->son_third_woman_img )
-<img class="img-thumbnail" src="{{ asset('storage/user/'. $names->son_third_woman_img) }}">
+<img class="img-thumbnail" src="{{ $names->son_third_woman_img_path }}">
 @else
 <img class="img-thumbnail" src="{{ asset('/sample_images/user_icon.png') }}">
 @endif
